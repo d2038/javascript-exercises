5fac98996c450ffaff4a972f3dc76bc89a139380
d2038 <35503394+d2038@users.noreply.github.com>
2022-03-30 17:11:02 -0400

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let formulaValue = (ftemp - 32) * 5 / 9;
+  let stringOneDecimal = formulaValue.toFixed(1);
+  return +stringOneDecimal;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  let formulaValue = ctemp * 9 / 5 + 32;
+  let stringOneDecimal = formulaValue.toFixed(1);
+  return +stringOneDecimal;
 };
 
 // Do not edit below this line
