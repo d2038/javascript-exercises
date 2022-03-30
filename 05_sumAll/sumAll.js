4a9bedb6b3d880adb3be7b7988879e712cf42023
d2038 <35503394+d2038@users.noreply.github.com>
2022-03-30 15:17:57 -0400

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || typeof num1 != "number" || 
+    typeof num2 != "number" ) {
+    return 'ERROR';    
+  }
 
+  let minNum = num1;
+  let maxNum = num2;
+
+  if (num1 > num2) {
+    minNum = num2;
+    maxNum = num1;    
+  }
+
+  let sum = 0;
+
+  for (let i = minNum; i <= maxNum; i++) {
+    sum += i;    
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
