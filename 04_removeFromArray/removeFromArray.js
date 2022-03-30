6ef93a6fbbf9e092aca60c9851f5130acc39f24c
d2038 <35503394+d2038@users.noreply.github.com>
2022-03-30 14:40:52 -0400

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  return array.filter( ele => !args.includes(ele) );
 };
 
 // Do not edit below this line
